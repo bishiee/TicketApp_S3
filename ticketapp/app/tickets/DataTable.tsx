1f46f4d4b98836ddf438595d90fdaafa7d81ef3f
BISHIE\Stranger <daulatrais101@gmail.com>
2024-03-28 18:03:01 +0700

    Ticket Status Badge

--- ticketapp/app/tickets/DataTable.tsx
+++ ticketapp/app/tickets/DataTable.tsx
@@ -1,3 +1,4 @@
+import TicketStatusBadge from "@/components/TicketStatusBadge";
 import {
   Table,
   TableBody,
@@ -22,7 +23,9 @@ const DataTable = ({ tickets }: Props) => {
           <TableHeader>
             <TableRow>
               <TableHead>Title</TableHead>
-              <TableHead>Status</TableHead>
+              <TableHead>
+                <div className="flex justify-center">Status</div>
+              </TableHead>
               <TableHead>Priority</TableHead>
               <TableHead>Created At</TableHead>
             </TableRow>
@@ -32,10 +35,21 @@ const DataTable = ({ tickets }: Props) => {
               ? tickets.map((ticket) => (
                   <TableRow key={ticket.id} data-href="/">
                     <TableCell>{ticket.title}</TableCell>
-                    <TableCell>{ticket.status}</TableCell>
+                    <TableCell>
+                      <div className="flex justify-center">
+                        <TicketStatusBadge status={ticket.status} />
+                      </div>
+                    </TableCell>
                     <TableCell>{ticket.priority}</TableCell>
                     <TableCell>
-                      {ticket.createdAt.toLocaleDateString()}
+                      {ticket.createdAt.toLocaleDateString("en-US", {
+                        year: "2-digit",
+                        month: "2-digit",
+                        day: "2-digit",
+                        hour: "numeric",
+                        minute: "2-digit",
+                        hour12: true,
+                      })}
                     </TableCell>
                   </TableRow>
                 ))
