250279a9be99a608a4d00665de80f7d7d61181b2
BISHIE\Stranger <daulatrais101@gmail.com>
2024-05-20 15:06:25 +0700

    NextAuth API, Navigation Login

--- ticketapp/components/MainNav.tsx
+++ ticketapp/components/MainNav.tsx
@@ -1,8 +1,12 @@
 import Link from "@/node_modules/next/link";
 import React from "react";
 import ToggleMode from "./ToggleMode";
+import { getServerSession } from "next-auth";
+import options from "@/app/api/auth/[...nextauth]/options";
 
-const MainNav = () => {
+const MainNav = async () => {
+  const session = await getServerSession(options);
+
   return (
     <div className="flex justify-between">
       <div className="flex items-center gap-2">
@@ -11,7 +15,11 @@ const MainNav = () => {
         <Link href="/users">Users</Link>
       </div>
       <div className="flex items-center gap-2">
-        <Link href="/">Logout</Link>
+        {session ? (
+          <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
         <ToggleMode />
       </div>
     </div>
